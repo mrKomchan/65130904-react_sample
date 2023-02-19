a4252093d4eb105c0d5fe7e8c35328876fcf4802
mrKomchan <komchan_m@outlook.com>
2023-02-19 10:10:28 +0700

    Update Pic

--- src/report.jsx
+++ src/report.jsx
@@ -8,6 +8,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import axios from "axios";
+import Image from 'react-bootstrap/Image';
 import Common from "./common";
 const BASE_URL = Common.API_URL;
 export default class report extends Component {
@@ -64,7 +65,10 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div style={{color:"#ffffff" , fontSize:"19px"}}>65130904 Komchan Rerkvises</div>
+            <div style={{color:"#ffffff" , fontSize:"19px"}}>
+              <Image src="https://scontent.fbkk22-7.fna.fbcdn.net/v/t39.30808-6/275322319_5222419981141421_8782643002804485384_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEYroV2f_ZkqN7H3Zr7GsDWg6QuIwnXl9yDpC4jCdeX3JuAcAfB5dLvqlqCW1q3fvKT3wuGNIwtn28qNvAtSk_0&_nc_ohc=3VHjljt_L4QAX9wWXpM&_nc_ht=scontent.fbkk22-7.fna&oh=00_AfBLfqHZK05Ou_9Q-Lf82C-6HLpoxZixLn16Sg5dnnEGCQ&oe=63F6FCA1"
+              roundedCircle style={{width:"50px",height:"50px"}}/>
+          {"  "} 65130904 Komchan Rerkvises</div>
           </Container>
         </Navbar>
         <Container>
